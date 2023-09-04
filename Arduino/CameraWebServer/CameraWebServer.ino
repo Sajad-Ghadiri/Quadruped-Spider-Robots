0b6b2a8fcb47d9354ca3ec1df9e140e250e073f3
Sajad Ghadiri <cjanthony.romeo@gmail.com>
2023-09-04 09:44:26 +0330

    Creating Camera Web Server with 2 button for LEDs to test Wifi module of ESP32CAM and connection

---
+++ Arduino/CameraWebServer/CameraWebServer.ino
@@ -0,0 +1,299 @@
+
+
+#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+
+#include "camera_pins.h"
+
+const char* ssid = "DLink";
+const char* password = "Sajad7258903";
+
+
+WiFiServer server(80);
+String header ; 
+
+String output26State = "off";
+String output27State = "off";
+
+const int output26 = 26;
+const int output27 = 27;
+
+
+void startCameraServer();
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  //                      for larger pre-allocated frame buffer.
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1); // flip it back
+    s->set_brightness(s, 1); // up the brightness just a bit
+    s->set_saturation(s, -2); // lower the saturation
+  }
+  // drop down frame size for higher initial frame rate
+  s->set_framesize(s, FRAMESIZE_QVGA);
+
+#if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+#endif
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+  server.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  WiFiClient client = server.available();     // Listen for incoming clients
+  if (client) { // If a new client connects,
+  Serial.println("New Client.");              // print a message out in the serial port
+  String currentLine = "";                    // make a String to hold incoming data from the client
+  while (client.connected()) {                // loop while the client's connected
+    if (client.available()) {                 // if there's bytes to read from the client,
+      char c = client.read();                 // read a byte, then
+      Serial.write(c);                        // print it out the serial monitor
+      header += c;
+      if (c == '\n') {                        // if the byte is a newline character
+      
+      // if the current line is blank, you got two newline characters in a row.
+      // that's the end of the client HTTP request, so send a response:
+        if (currentLine.length() == 0) {
+        // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+        // and a content-type so the client knows what's coming, then a blank line:
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-type:text/html");
+          client.println("Connection: close");
+          client.println();
+
+          // turns the GPIOs on and off
+          if (header.indexOf("GET /26/on") >= 0) {
+            Serial.println("GPIO 26 on");
+            output26State = "on";
+            digitalWrite(output26, HIGH);
+          } else if (header.indexOf("GET /26/off") >= 0) {
+            Serial.println("GPIO 26 off");
+            output26State = "off";
+            digitalWrite(output26, LOW);
+          } else if (header.indexOf("GET /27/on") >= 0) {
+            Serial.println("GPIO 27 on");
+            output27State = "on";
+            digitalWrite(output27, HIGH);
+          } else if (header.indexOf("GET /27/off") >= 0) {
+            Serial.println("GPIO 27 off");
+            output27State = "off";
+            digitalWrite(output27, LOW);
+          }
+
+          // Display HTML 
+          client.println("<!DOCTYPE HTML><html>");
+          client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+          client.println("<link rel=\"icon\" href=\"data:,\">");
+          client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+          client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+          client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+          client.println(".button2 {background-color: #555555;}</style></head>");
+
+          // Web Page Heading
+          client.println("<h1>ESP32 Web Server</h1>");
+
+          // Displaying the 26 Button and Corresponding State
+          client.println("<p>GPIO 26 - State " + output26State + "</p>");
+          if (output26State=="off") {
+            client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
+          } else {
+            client.println("<p><a href=\"/26/off\"><button class=\"button button1\">OFF</button></a></p>");
+          }
+
+          // Displaying the 27 Button and Corresponding State
+          client.println("<p>GPIO 27 - State " + output27State + "</p>");
+          if (output26State=="off") {
+            client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
+          } else {
+            client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
+          }
+          
+          // Clear the header variable
+          header = "";
+          // Close the connection
+          client.stop();
+          }
+      }
+    }
+  }
+  }
+delay(10000);
+}
+ */
+ #include "esp_camera.h"
+#include <WiFi.h>
+
+//
+// WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
+//            Ensure ESP32 Wrover Module or other board with PSRAM is selected
+//            Partial images will be transmitted if image exceeds buffer size
+//
+
+// Select camera model
+//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
+//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
+#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
+
+#include "camera_pins.h"
+
+const char* ssid = "IRLab";
+const char* password = "Khosravi503";
+
+void startCameraServer();
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  //                      for larger pre-allocated frame buffer.
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1); // flip it back
+    s->set_brightness(s, 1); // up the brightness just a bit
+    s->set_saturation(s, -2); // lower the saturation
+  }
+  // drop down frame size for higher initial frame rate
+  s->set_framesize(s, FRAMESIZE_QVGA); // QVGA
+
+#if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+#endif
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(10000);
+} 
